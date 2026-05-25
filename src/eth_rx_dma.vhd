15987e9fdb994499a47e6ccbeca424d6323c46fc
Ameen Shaikh <ameen.shaikh@annapmicro.com>
2026-05-25 18:34:57 -0400

    Basic DMA src code (incomplete)

--- src/eth_rx_dma.vhd
+++ src/eth_rx_dma.vhd
@@ -52,29 +52,69 @@ entity eth_rx_dma is
         -- AXI master write interface --------------
         -- Allows DMA to perform writes to memory --
         --------------------------------------------
-        M_AXI_ACLK    : in  std_logic;
-        M_AXI_ARESETN : in  std_logic;
+        M_AXI_ACLK : in std_logic;
+        M_AXI_ARESETN : in std_logic;
 
         -- AXI write address channel
         M_AXI_AWADDR  : out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
         M_AXI_AWPROT  : out std_logic_vector(2 downto 0);
         M_AXI_AWVALID : out std_logic;
-        M_AXI_AWREADY : in  std_logic;
+        M_AXI_AWREADY : in std_logic;
 
         -- AXI write data channel
-        M_AXI_WDATA   : out std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
-        M_AXI_WSTRB   : out std_logic_vector((C_M_AXI_DATA_WIDTH/8)-1 downto 0);
-        M_AXI_WVALID  : out std_logic;
-        M_AXI_WREADY  : in  std_logic;
+        M_AXI_WDATA : out std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
+        M_AXI_WSTRB : out std_logic_vector((C_M_AXI_DATA_WIDTH/8)-1 downto 0);
+        M_AXI_WVALID : out std_logic;
+        M_AXI_WREADY : in  std_logic;
 
         -- AXI write response channel
-        M_AXI_BRESP   : in  std_logic_vector(1 downto 0);
-        M_AXI_BVALID  : in  std_logic;
-        M_AXI_BREADY  : out std_logic
+        M_AXI_BRESP : in  std_logic_vector(1 downto 0);
+        M_AXI_BVALID : in  std_logic;
+        M_AXI_BREADY : out std_logic
     );
 end entity;
 
 architecture rtl of eth_rx_dma is
+    signal dma_clk : std_logic; 
+    signal dma_resetn : std_logic;
+
+    type t_dma_state is
+        ( s_A, s_B, );
+    signal state_reg : t_dma_state;
+    signal state_next : t_dma_state;
 begin
+
+    dma_clk <= S_AXI_ACLK;
+    dma_resetn <= S_AXI_ARESETN;
+
+    process(dma_clk) is
+    begin
+        if rising_edge(dma_clk) then
+            if (dma_resetn = '0') then
+
+            else
+
+            end if;
+        end if;
+    end process;
+
+    process(all) is
+    begin
+
+        -- Defaults
+        state_next <= state_reg;
+
+        case state_reg is
+
+            when s_A =>
+                if (i_rx_fifo_empty = '0') then
+                    o_rx_fifo_rd_en <= '1'; 
+                    -- Read latency is 1, so need to wait until next cycle to read 'i_rx_fifo_dout'
+                    state_next <= s_B;
+                end if;
+
+            when
+        end case;
+    end process;
 
 end architecture;
